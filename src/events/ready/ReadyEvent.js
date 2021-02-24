7335f0c032d47452e562c97951880c3d9b32f9f0
Bernipaley <paleybernardo@gmail.com>
2021-02-23 23:42:11 -0300

    update

--- src/events/ready/ReadyEvent.js
+++ src/events/ready/ReadyEvent.js
@@ -5,7 +5,7 @@ module.exports = class ReadyEvent extends BaseEvent {
     super('ready');
   }
   async run (client) {
-    console.log(`${client.user.tag} has logged in. Also im ready!, in ${client.guilds.cache.size} servers | and i help  ${client.users.cache.size} Users`);
+    console.log(`${client.user.tag} has logged in.`);
     client.user.setPresence({
       activity: {
         name: 'X-Plane 11',

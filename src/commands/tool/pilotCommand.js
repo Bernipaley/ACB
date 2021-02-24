d4c42d85d0115ac28b1b9456f32d8032e9ca85db
Bernipaley <paleybernardo@gmail.com>
2021-02-24 19:07:46 -0300

    Update

--- src/commands/tool/pilotCommand.js
+++ src/commands/tool/pilotCommand.js
@@ -1,14 +1,14 @@
 const BaseCommand = require('../../utils/structures/BaseCommand');
 
-module.exports = class VpCommand extends BaseCommand {
+module.exports = class PilotCommand extends BaseCommand {
   constructor() {
-    super('vp', 'tool', []);
+    super('pilot', 'tool', []);
   }
 
   async run(client, message, args) {
      if (!message.guild.me.hasPermission("MANAGE_ROLES")) return message.channel.send ("🛑 I DONT HAVE MANAGE ROLES PERMISSION 🛑");
 
-     const role = message.guild.roles.cache.get('801623868787785738');  message.channel.send ("Virtual pilot role added")
+     const role = message.guild.roles.cache.get('801623868787785738');  message.channel.send ("Pilot role added")
 
      await message.member.roles.add(role.id).catch(err => console.log(err));
   }

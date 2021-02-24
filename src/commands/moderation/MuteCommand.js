5bac7b11c36076e2d533aa4d84448a54955a0480
Bernipaley <paleybernardo@gmail.com>
2021-02-24 10:38:26 -0300

    Update MuteCommand.js

--- src/commands/moderation/MuteCommand.js
+++ src/commands/moderation/MuteCommand.js
@@ -11,7 +11,7 @@ module.exports = class MuteCommand extends BaseCommand {
     if (!message.guild.me.hasPermission("MUTE_MEMBERS")) return message.channel.send('🛑YOU DONT HAVE PERMISSION TO USE THIS COMMAND🛑');
     let reason = args.slice(1).join(" ");
     const muteRole = message.guild.roles.cache.get('814105079845879838');
-    const memberRole = message.guild.roles.cache.get('813034318053507094','801623868787785738','801586686516789248','801586636651626518','801586503129104454');
+    const memberRole = message.guild.roles.cache.get('813034318053507094');
     const mentionedMember = message.mentions.members.first || message.guild.members.cache.get(args[0]);
     const muteEmbed = new Discord.MessageEmbed()
       .setTitle(`You were muted at ${message.guild.name}`)
